b543625170b887ab609a06b4e12804325f3dae6d
Mykhailo Hrynko <mykhailo.hrynko@solidgate.com>
2026-01-24 18:00:22 +0200

    fix: topic ids and sizes

--- grinder/config/topics.js
+++ grinder/config/topics.js
@@ -10,13 +10,13 @@ export let topicsMap = {
 }
 
 export let topics = {
-	'Big picture': { id: 1, max: 6 },
-	'America':  { id: 2, max: 24 },
+	'Big picture': { id: 1, max: 12 },
+	'America':  { id: 2, max: 30 },
 	'Left Is losing it':  { id: 3, max: 6 },
 	'Ukraine':  { id: 4, max: 24 },
-	'Гадание на кофе':  { id: 5, max: 6 },
+	'Гадание на кофе':  { id: 5, max: 9 },
 	'World news':  { id: 6, max: 24 },
-	'Маразм крепчал':  { id: 8, max: 6 },
-	'Tech News':  { id: 9, max: 6 },
-	'Crazy news':  { id: 10, max: 6 },
+	'Маразм крепчал':  { id: 7, max: 6 },
+	'Tech News':  { id: 8, max: 6 },
+	'Crazy news':  { id: 9, max: 6 },
 }
